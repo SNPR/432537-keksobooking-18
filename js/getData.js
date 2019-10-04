a604c20c04226516f64aa00d5a7035640daade41
snpr <vaugk13@gmail.com>
2019-10-04 13:57:00 +0500

    Create getDate module

---
+++ js/getData.js
@@ -0,0 +1,33 @@
+'use strict';
+(function () {
+  var MAX_TIMEOUT_TIME = 10000;
+
+  window.getData = function (url, onSuccess, onError) {
+    var xhr = new XMLHttpRequest();
+
+    xhr.responseType = 'json';
+
+    xhr.addEventListener('load', function () {
+      if (xhr.status === 200) {
+        onSuccess(xhr.response);
+      } else {
+        onError('Cтатус ответа: ' + xhr.status + ' ' + xhr.statusText);
+      }
+    });
+
+    xhr.addEventListener('error', function () {
+      onError('Произошла ошибка соединения');
+    });
+
+    xhr.addEventListener('timeout', function () {
+      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+    });
+
+    xhr.timeout = MAX_TIMEOUT_TIME;
+
+    xhr.open('GET', url);
+    xhr.send();
+  };
+})();
+
+
