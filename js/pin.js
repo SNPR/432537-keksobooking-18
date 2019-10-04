5fdd432fc8dbbbc4ea2cfe78608669b18b2456bf
snpr <vaugk13@gmail.com>
2019-10-04 14:57:25 +0500

    Get data from API

--- js/pin.js
+++ js/pin.js
@@ -4,23 +4,27 @@
   var pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
   var map = document.querySelector('.map');
 
-  window.renderPin = function (advertisement, index) {
-    var pin = pinTemplate.cloneNode(true);
-    var pinImage = pin.querySelector('img');
+  var onSuccess = function (advertisements) {
+    window.renderPin = function (advertisement, index) {
+      var pin = pinTemplate.cloneNode(true);
+      var pinImage = pin.querySelector('img');
 
-    pin.style = 'left: ' + advertisement.location.x + 'px; top: ' + advertisement.location.y + 'px';
-    pinImage.src = advertisement.author.avatar;
-    pinImage.alt = advertisement.offer.title;
+      pin.style = 'left: ' + advertisement.location.x + 'px; top: ' + advertisement.location.y + 'px';
+      pinImage.src = advertisement.author.avatar;
+      pinImage.alt = advertisement.offer.title;
 
-    pin.addEventListener('click', function () {
-      var advertisementCard = map.querySelector('.map__card');
+      pin.addEventListener('click', function () {
+        var advertisementCard = map.querySelector('.map__card');
 
-      if (advertisementCard) {
-        map.removeChild(advertisementCard);
-      }
-      map.appendChild(window.renderAdvertisementCard(window.advertisements[index]));
-    });
+        if (advertisementCard) {
+          map.removeChild(advertisementCard);
+        }
+        map.appendChild(window.renderAdvertisementCard(advertisements[index]));
+      });
 
-    return pin;
+      return pin;
+    };
   };
+
+  window.getData('https://js.dump.academy/keksobooking/data', onSuccess);
 })();
